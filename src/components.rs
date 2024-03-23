1e226b787f5d22ea6cef5fdc4b291960a77eab75
primitiveType <arthur.littlefield@gmail.com>
2024-03-23 09:40:33 -0700

    cleanup warnings

--- src/components.rs
+++ src/components.rs
@@ -1,21 +1,16 @@
-use bevy::asset::Handle;
-use bevy::audio::AudioSource;
-use bevy::math::Vec2;
-use bevy::prelude::{Bundle, ColorMaterial, Component, default, Event, Reflect, Resource, Sprite, SpriteBundle, Timer, Transform};
-use bevy::sprite::{MaterialMesh2dBundle, SpriteSheetBundle};
-use bevy::time::TimerMode;
+use bevy::prelude::*;
+use bevy::prelude::Component;
+use bevy::prelude::Event;
+use bevy::prelude::Reflect;
 use bevy_asepritesheet::prelude::AnimatedSpriteBundle;
 use bevy_xpbd_2d::components::{CollisionLayers, Friction, LinearVelocity, Mass, Restitution, RigidBody};
 use bevy_xpbd_2d::prelude::Collider;
 use serde::Deserialize;
 use serde::Serialize;
 
-use crate::constants::{BOTTOM_WALL, LEFT_WALL, RIGHT_WALL, TOP_WALL, WALL_COLOR, WALL_THICKNESS};
-use crate::physics::layers::GameLayer;
-
 #[derive(Component, Default)]
 pub struct Player {
-    pub xp: u16,
+    // pub xp: u16,
     pub level: u16,
 }
 
@@ -90,11 +85,17 @@ pub struct GainXPOnTouch {
     pub value: u16,
 }
 
+#[derive(Component)]
+pub struct XP {
+    pub amount: u16,
+}
+
+#[derive(Component)]
+pub struct XPVacuum {}
+
 #[derive(Event, Default)]
 pub struct CollisionEvent;
 
-#[derive(Resource)]
-pub struct CollisionSound(pub Handle<AudioSource>);
 
 // This bundle is a collection of the components that define a "wall" in our game
 #[derive(Bundle)]
@@ -109,88 +110,12 @@ pub struct BulletBundle {
     pub bullet: Bullet,
     pub mass: Mass,
     pub linear_velocity: LinearVelocity,
+    pub name: Name,
 }
 
 
-// This bundle is a collection of the components that define a "wall" in our game
-#[derive(Bundle)]
-pub struct WallBundle {
-    // You can nest bundles inside of other bundles like this
-    // Allowing you to compose their functionality
-    pub sprite_bundle: SpriteBundle,
-    pub collider: Collider,
-    pub rigid_body: RigidBody,
-    pub friction: Friction,
-    pub restitution: Restitution,
-    pub mask: CollisionLayers,
-}
 
-/// Which side of the arena is this wall located on?
-pub enum WallLocation {
-    Left,
-    Right,
-    Bottom,
-    Top,
-}
 
-impl WallLocation {
-    fn position(&self) -> Vec2 {
-        match self {
-            WallLocation::Left => Vec2::new(LEFT_WALL, 0.),
-            WallLocation::Right => Vec2::new(RIGHT_WALL, 0.),
-            WallLocation::Bottom => Vec2::new(0., BOTTOM_WALL),
-            WallLocation::Top => Vec2::new(0., TOP_WALL),
-        }
-    }
-
-    fn size(&self) -> Vec2 {
-        let arena_height = TOP_WALL - BOTTOM_WALL;
-        let arena_width = RIGHT_WALL - LEFT_WALL;
-        // Make sure we haven't messed up our constants
-        assert!(arena_height > 0.0);
-        assert!(arena_width > 0.0);
-
-        match self {
-            WallLocation::Left | WallLocation::Right => {
-                Vec2::new(WALL_THICKNESS, arena_height + WALL_THICKNESS)
-            }
-            WallLocation::Bottom | WallLocation::Top => {
-                Vec2::new(arena_width + WALL_THICKNESS, WALL_THICKNESS)
-            }
-        }
-    }
-}
-
-impl WallBundle {
-    // This "builder method" allows us to reuse logic across our wall entities,
-    // making our code easier to read and less prone to bugs when we change the logic
-    pub fn new(location: WallLocation) -> WallBundle {
-        WallBundle {
-            sprite_bundle: SpriteBundle {
-                transform: Transform {
-                    // We need to convert our Vec2 into a Vec3, by giving it a z-coordinate
-                    // This is used to determine the order of our sprites
-                    translation: location.position().extend(0.0),
-                    // The z-scale of 2D objects must always be 1.0,
-                    // or their ordering will be affected in surprising ways.
-                    // See https://github.com/bevyengine/bevy/issues/4149
-                    scale: location.size().extend(1.0),
-                    ..default()
-                },
-                sprite: Sprite {
-                    color: WALL_COLOR,
-                    ..default()
-                },
-                ..default()
-            },
-            collider: Collider::rectangle(1.0, 1.0),
-            rigid_body: RigidBody::Static,
-            friction: Friction::ZERO,
-            restitution: Restitution::new(1.0),
-            mask: CollisionLayers::new(GameLayer::Ground, [GameLayer::Ball, GameLayer::Player, GameLayer::Enemy]),
-        }
-    }
-}
 
 
 #[derive(Component)]
--- src/systems/spawning.rs
+++ src/systems/spawning.rs
@@ -1,5 +1,4 @@
-use bevy::asset::AssetServer;
-use bevy::prelude::{Commands, Query, Res, ResMut};
+use bevy::prelude::{Commands, Query, ResMut};
 pub use bevy::utils::petgraph::visit::Walker;
 use bevy_prng::WyRand;
 use bevy_rand::prelude::GlobalEntropy;
@@ -11,15 +10,14 @@ use crate::initialization::load_prefabs::Atlases;
 pub fn enemy_spawn_cycle(
     query: Query<&Enemy>,
     _commands: Commands,
-    asset_server: Res<AssetServer>,
     atlases: ResMut<Atlases>,
-    mut rng: ResMut<GlobalEntropy<WyRand>>,
+    rng: ResMut<GlobalEntropy<WyRand>>,
 ) {
     let count = query.iter().len();
 
 
-    if count < 200 {
+    if count < 1 {
         println!("Spawning.");
-        spawn_enemy(1, _commands, asset_server, atlases, rng);
+        spawn_enemy(1, _commands, atlases, rng);
     }
 }
