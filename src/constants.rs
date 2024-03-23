4bcdf6003b731f41e7977511e907541d79e0726a
primitiveType <arthur.littlefield@gmail.com>
2024-03-23 09:42:55 -0700

    cleanup warnings

--- src/constants.rs
+++ src/constants.rs
@@ -8,8 +8,6 @@ pub const PLAYER_SPEED: f32 = 50_000.0;
 // How close can the paddle get to the wall
 pub const PADDLE_PADDING: f32 = 10.0;
 
-// We set the z-value of the ball to 1 so it renders on top in the case of overlapping sprites.
-pub const ENEMY_STARTING_POSITION: Vec3 = Vec3::new(0.0, 0.0, 0.0);
 pub const XP_DIAMETER: f32 = 5.;
 
 pub const WALL_THICKNESS: f32 = 10.0;
@@ -25,8 +23,5 @@ pub const SCOREBOARD_FONT_SIZE: f32 = 40.0;
 pub const SCOREBOARD_TEXT_PADDING: Val = Val::Px(20.0);
 
 pub const BACKGROUND_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
-pub const PADDLE_COLOR: Color = Color::rgb(0.3, 0.3, 0.7);
-pub const BALL_COLOR: Color = Color::rgb(1.0, 0.5, 0.5);
-pub const WALL_COLOR: Color = Color::rgb(0.8, 0.8, 0.8);
 pub const TEXT_COLOR: Color = Color::rgb(0.5, 0.5, 1.0);
 pub const SCORE_COLOR: Color = Color::rgb(1.0, 0.5, 0.5);
