39fb29bb1c81c77d3c65ac159b57fe5b9d5a8aa3
primitiveType <arthur.littlefield@gmail.com>
2024-04-13 12:35:02 -0700

    add macro for temporary components

---
+++ temporary_component_derive/src/lib.rs
@@ -0,0 +1,25 @@
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
+
+#[proc_macro_derive(TemporaryComponent)]
+pub fn hello_world_derive(input: TokenStream) -> TokenStream {
+    let ast = parse_macro_input!(input as DeriveInput);
+    let name = &ast.ident;
+
+    let gen = quote! {
+        impl TemporaryComponent for #name {
+            fn advance_timer(&mut self, duration : Duration) {
+                self.timer.tick(duration);
+            }
+
+            fn is_finished(&self) -> bool {
+                self.timer.finished()
+            }
+        }
+    };
+
+    gen.into()
+}
